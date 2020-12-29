fdbd98365175992191a7979eb58c0055cb6698f5
shoneriki <ikirenohs@gmail.com>
2020-12-29 18:26:37 +0900

    array for images

---
+++ app.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', () => {
+  //cards
+  const cardArray = [
+    {
+      name: 'fries',
+      img: 'images/fries.jpg'
+    },
+    {
+      name: 'fries',
+      img: 'images/fries.jpg'
+    },
+    {
+      name: 'cheeseburger',
+      img: 'images/cheeseburger.jpg'
+    },
+    {
+      name: 'cheeseburger',
+      img: 'images/cheeseburger.jpg'
+    },
+    {
+      name: 'hotdog',
+      img: 'images/hotdog.jpg'
+    },
+    {
+      name: 'hotdog',
+      img: 'images/hotdog.jpg'
+    },
+    {
+      name: 'ice-cream',
+      img: 'images/ice-cream.jpg'
+    },
+    {
+      name: 'ice-cream',
+      img: 'images/ice-cream.jpg'
+    },
+    {
+      name: 'milkshake',
+      img: 'images/milkshake.jpg'
+    },
+    {
+      name: 'milkshake',
+      img: 'images/milkshake.jpg'
+    },
+    {
+      name: 'pizza',
+      img: 'images/pizza.jpg'
+    },
+    {
+      name: 'pizza',
+      img: 'images/pizza.jpg'
+    },
+  ]
+
+})
